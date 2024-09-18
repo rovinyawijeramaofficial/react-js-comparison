a8cc572301e5eee75a627b2fcfcccec8b6ff5aea
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-18 13:38:00 +0530

    Updated

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,15 +1,28 @@
-function Todo(props){
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
 
-    function showModalHandler() {
-       
-      }
+    function Todo(props) {
+        const [modalIsOpen, setModalIsOpen] = useState(false);
+      
+        function deleteHandler() {
+          setModalIsOpen(true);
+        }
+
+        function closeModalHandler() {
+           setModalIsOpen(false);
+          }
 
     return (
     <div className='card'>
     <h2>{props.text}</h2>
     <div className='actions'> 
-      <button className='btn' onClick={showModalHandler}>Delete</button>
+      <button className='btn' onClick={deleteHandler}>Delete</button>
     </div>
+    {modalIsOpen && (
+        <Modal onClose={closeModalHandler}/>
+    )}
+    {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
   </div>
     );
 }
