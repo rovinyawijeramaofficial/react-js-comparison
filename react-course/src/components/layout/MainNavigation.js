e593b7d0e032fd0cee5105c52c16bffb052a1f4e
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-19 15:40:02 +0530

    Updated

--- react-course/src/components/layout/MainNavigation.js
+++ react-course/src/components/layout/MainNavigation.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
-const MainNavigation = () => {
+function MainNavigation() {
   return (
-    <div>
-        <h1>MainNavigation</h1>
-    </div>
-  )
+    <header className={classes.header}>
+    <div className={classes.logo}> React Meetups</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>All Meetups</Link>
+          </li>
+          <li>
+          <Link to='/new-meetup'>Add New Meetups</Link>
+          </li>
+          <li>
+          <Link to='/favorites'>My Favorites</Link>
+          </li>  
+      </ul>
+    </nav>
+    </header>
+  );
 }
 
-export default MainNavigation
+export default MainNavigation;
