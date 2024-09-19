89e123dcfc21be55e2a8fd37a7c40fddb65f9099
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-20 04:13:58 +0530

    Updated

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,21 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import React from 'react';
-
+import Layout from "./components/layout/Layout";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from './components/layout/MainNavigation';
+
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
