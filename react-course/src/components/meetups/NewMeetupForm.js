cad2e219b2c084dc3a5592e676a4a806da85da88
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-20 09:08:46 +0530

    Updated

---
+++ react-course/src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,34 @@
+import Card from '../ui/Card';
+import React from 'react';
+import classes from './NewMeetupForm.module.css';
+
+
+function NewMeetupForm(){
+    return (
+    <Card>
+        <form className={classes.form}>
+            <div className={classes.control}>
+                <label htmlFor='title'>Meetup Title</label>
+                <input type='text' required id='title'/>
+                </div>
+            <div className={classes.control}>
+                <label htmlFor='image'>Meetup Image</label>
+                <input type='url' required id='image'/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='address'>Address</label>
+                <input type="text" required id='address'/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='description'>Description</label>
+                <textarea id="description">
+                <input type="url" required rows='5' />
+                </textarea>
+            </div>
+        </form>
+        </Card>   
+    );
+}
+
+
+export default NewMeetupForm;
