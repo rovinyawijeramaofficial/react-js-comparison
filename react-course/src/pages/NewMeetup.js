59e4e1ec1287b25a88992dad03f8f15259618e38
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-20 09:09:05 +0530

    Updated

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+
 
 function NewMeetupPage() {
     return (
-    <div>New Meetup Page</div>
+    <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm/>
+    </section>
     );
 }
 
