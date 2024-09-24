444b96603ee8f89590a66e3f71447403a0f55b22
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-24 13:01:46 +0530

    Updated

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
+import { FavoritesContextProvider } from './store/favorites-context';
 
+// Create the root of the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app with the FavoritesContextProvider wrapping BrowserRouter and App
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>
 );
